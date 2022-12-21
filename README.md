ea001575931dea70b5e5473bb72d4c55be78c44a
daerhiel <485120+daerhiel@users.noreply.github.com>
2022-12-21 19:09:26 +0100

    init(Domain): Let there be light

---
+++ README.md
@@ -0,0 +1,3 @@
+A C# response to [python_async_playground](https://github.com/mjh-sakh/python_async_playground) vario speed Python implementation.
+
+It seems like a violation of parallel / async .NET implementation patterns but done so just for the sake of replicating the same single-threadey event-loopy behavior.
---
+++ münch-hausen/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Concurrent;
+
+var actor1 = new Actor(0.3);
+var actor2 = new Actor(0.8);
+
+var source = new CancellationTokenSource(TimeSpan.FromSeconds(5));
+var context = new ConcurrentDictionary<Actor, Task>();
+context.TryAdd(actor1, Task.CompletedTask);
+context.TryAdd(actor2, Task.CompletedTask);
+
+Console.WriteLine("S\tF");
+do
+{
+    foreach (var (actor, task) in context)
+    {
+        if (task.IsCompleted)
+            context[actor] = actor.Act(source.Token).ContinueWith(task =>
+            {
+                if (!task.IsCanceled)
+                    Console.WriteLine($"{(actor.Name != "fast" ? '\t' : string.Empty)}{actor.State}");
+            });
+    }
+    await Task.WhenAny(context.Values);
+}
+while (!source.IsCancellationRequested);
+
+public class Actor
+{
+    private readonly double _speed;
+    private readonly string _name;
+    private int _state;
+
+    public string Name => _name;
+    public int State => _state;
+
+    public Actor(double speed)
+    {
+        _speed = speed;
+        _name = speed > 0.5 ? "fast" : "slow";
+    }
+
+    public Task Act(CancellationToken cancellationToken = default)
+    {
+        var task = Task.Delay(TimeSpan.FromSeconds(_speed), cancellationToken);
+        _state++;
+        return task;
+    }
+}
